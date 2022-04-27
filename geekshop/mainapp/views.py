94da97da5968da7b4a01292de3f089cbb6cc56dd
Damir <damir.urmanov2003@mail.ru>
2022-04-27 15:37:14 +0500

    lesson_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
+import json
+
+with open('data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
 
-# Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'menu': data['menu'],
+        'social_links': data['social_links'],
+    })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context={
+        'menu': data['menu'],
+        'social_links': data['social_links'],
+        'products': data['products'],
+        'products_links': data['products_links'],
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={
+        'menu': data['menu'],
+        'social_links': data['social_links'],
+    })
